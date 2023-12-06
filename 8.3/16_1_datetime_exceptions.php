a0cf293f7c04149a36526e626793c1a1f8602778
Alexis Wurth <alexis.wurth@sensiolabs.com>
2023-12-06 12:41:51 +0100

    add comments

--- 8.3/16_1_datetime_exceptions.php
+++ 8.3/16_1_datetime_exceptions.php
@@ -1,13 +1,13 @@
 <?php
 
 try {
-    new DateInterval(duration: 'foo');
+    new DateInterval(duration: 'foo'); // Exception becomes DateMalformedIntervalStringException
 } catch (DateMalformedIntervalStringException $e) {
     var_dump($e);
 }
 
 try {
-    (new DateTimeImmutable())->sub(interval: DateInterval::createFromDateString('next weekday'));
+    (new DateTimeImmutable())->sub(interval: DateInterval::createFromDateString('next weekday')); // Warning becomes Exception
 } catch (DateInvalidOperationException $e) {
     var_dump($e);
 }
